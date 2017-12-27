1a32dd52c097df46bb7355dc39164da7194acb3c
Randall Alexander <Randall.Alexander@gmail.com>
2017-12-27 10:11:42 -0800

    Ch 14 Testing - Addition

---
+++ proj/addition/Addition.hs
@@ -0,0 +1,20 @@
+module Addition where
+
+import Test.Hspec
+import Test.QuickCheck
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where go n  d count
+         | n < d = (count, n)
+         | otherwise = go(n - d) d (count + 1)
+
+main :: IO ()
+main = hspec $ do
+  describe "dividedBy" $ do
+    it "15 divided by 3 is 5" $ do
+      dividedBy 15 3 `shouldBe` (5, 0)
+    it "22 divided 5 is 4 remainder 2" $ do
+      dividedBy 22 5 `shouldBe` (4, 2)
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
